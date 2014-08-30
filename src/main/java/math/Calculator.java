32b2e998333c22020569b247852b644a3c35aadd
Stefan Zoerner <stefan.zoerner@embarc.de>
2014-08-30 19:31:34 +0200

    Tests added

--- src/main/java/math/Calculator.java
+++ src/main/java/math/Calculator.java
@@ -7,4 +7,5 @@ public class Calculator {
     public int plus(int a, int b) {
         return a + b;
     }
+
 }
---
+++ src/test/java/math/CalculatorTest.java
@@ -0,0 +1,17 @@
+package math;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+/**
+ * Created by stefanz on 30.08.14.
+ */
+public class CalculatorTest {
+
+    @Test
+    public void addTwoNumbers() {
+        Calculator calc = new Calculator();
+        int result = calc.plus(5, 7);
+        Assert.assertEquals(12, result);
+    }
+}
